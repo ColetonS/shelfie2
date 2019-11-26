ad0c942afa33f699053a1872d4f43e8bd45c53cc
ColetonS <coleton.m.smith@gmail.com>
2019-11-26 08:42:42 -0700

    inputs and buttons on Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Form extends Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
 
-    render() {
-        return (
-            <div>Form</div>
-        )
+    this.state = {
+        image: '',
+        productName: '',
+        price: 0
     }
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <div className='form-inputs'>
+          <h2>Image URL</h2>
+          <input type='text'/>
+          <h2>Product Name</h2>
+          <input type='text'/>
+          <h2>Price</h2>
+          <input type='number'/>
+        </div>
+        <div className="form-buttons">
+          <button>Cancel</button>
+          <button>Add to Inventory</button>
+        </div>
+      </div>
+    );
+  }
 }
